30ff14a47adbd80fed9c0e296c376c27ae5affaa
Ajit Patil <patilajit020@gmail.com>
2022-11-05 21:49:58 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <header className="text-gray-600 body-font">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link to={"/"} className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span className="ml-3 text-xl">World Wide News</span>
+            <span className="ml-3 text-xl">Get Instant News</span>
           </Link>
           <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
             <Link to={"/business"} className="mr-5 hover:text-gray-900">
